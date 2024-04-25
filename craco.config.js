6bd778b00f9d8cb03f0ee6220a27e9e2403d7cdf
Tom Haile <bradford.haile@gmail.com>
2024-04-25 12:17:45 -0500

    remove refresh in production

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,27 @@
-// craco.config.js
+const { whenDev } = require("@craco/craco");
+
 module.exports = {
     webpack: {
-      configure: (webpackConfig, { env, paths }) => {
-        webpackConfig.resolve.fallback = {
-          ...webpackConfig.resolve.fallback, // retain existing fallbacks
-          crypto: require.resolve('crypto-browserify'),
-          stream: require.resolve('stream-browserify'),
-        };
-        return webpackConfig;
-      },
+        configure: (webpackConfig, { env, paths }) => {
+            webpackConfig.resolve.fallback = {
+                ...webpackConfig.resolve.fallback,
+                crypto: require.resolve('crypto-browserify'),
+                stream: require.resolve('stream-browserify'),
+            };
+            return webpackConfig;
+        },
     },
-  };
-  
+    babel: {
+        plugins: [
+            ...whenDev(() => [["react-refresh/babel", {}, "react-refresh-babel-plugin"]], [])
+        ]
+    },
+    plugins: [
+        ...whenDev(() => [{
+            plugin: require('@pmmmwh/react-refresh-webpack-plugin'),
+            options: {
+                disableRefreshCheck: true,
+            }
+        }], [])
+    ]
+};
