2f80e0955b693dd0336eec1fcf9d198872b364e0
Tom Haile <bradford.haile@gmail.com>
2024-04-25 12:22:33 -0500

    turn off refresh in production

--- craco.config.js
+++ craco.config.js
@@ -1,27 +1,22 @@
 const { whenDev } = require("@craco/craco");
 
 module.exports = {
+    babel: {
+        plugins: [
+            ...whenDev(() => ["react-refresh/babel"], []), // Ensures it's only included in development
+        ]
+    },
     webpack: {
         configure: (webpackConfig, { env, paths }) => {
+            if (env === "development") {
+                const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+                webpackConfig.plugins.push(new ReactRefreshWebpackPlugin());
+            }
             webpackConfig.resolve.fallback = {
-                ...webpackConfig.resolve.fallback,
                 crypto: require.resolve('crypto-browserify'),
                 stream: require.resolve('stream-browserify'),
             };
             return webpackConfig;
         },
     },
-    babel: {
-        plugins: [
-            ...whenDev(() => [["react-refresh/babel", {}, "react-refresh-babel-plugin"]], [])
-        ]
-    },
-    plugins: [
-        ...whenDev(() => [{
-            plugin: require('@pmmmwh/react-refresh-webpack-plugin'),
-            options: {
-                disableRefreshCheck: true,
-            }
-        }], [])
-    ]
 };
